d6985291cf9085694435b9d152aba17e5c2ddd17
Kara Rainey <karain82@hotmail.com>
2018-02-12 17:06:14 -0600

    finished

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Provider} from 'react-redux';
+import {HashRouter} from 'react-router-dom';
+import store from './store';
+
+ReactDOM.render(
+    <Provider store = {store}>
+      <HashRouter>
+         <App />
+      </HashRouter>   
+    </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
 
